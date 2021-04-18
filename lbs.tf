f8af31923e55c03734720a3257c045c90c542bd1
ronkaiser86@gmail.com <ron.kaiser@cellwize.com>
2021-04-19 00:27:25 +0300

    modify lbs.tf file

--- lbs.tf
+++ lbs.tf
@@ -1,9 +1,9 @@
 # Public Load Balancer
 resource "azurerm_lb" "publiclb" {
   name = "PublicLB"
-  location                      = var.location
+  location            = var.location
   resource_group_name = var.rg
-  sku                           = "Standard"
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
@@ -12,9 +12,16 @@ resource "azurerm_lb" "publiclb" {
 }
 
 # Public Load Balancer backend pool
-resource "azure_lb_backend_address_pool" "publiclbpool" {
-    loadbalancer_id = azurerm_lb.publiclb.id
-    name            = "FrontEndAddressPool"
+resource "azurerm_lb_backend_address_pool" "publiclbpool" {
+  loadbalancer_id     = azurerm_lb.publiclb.id
+  name                = "BackEndAddressPool"
+  resource_group_name = var.rg
 }
 
-# Public Load Balancer Pool
+# Public Load Balancer Pool associations
+resource "azurerm_network_interface_backend_address_pool_association" "webpool" {
+  for_each                = toset(var.app_vm)
+  network_interface_id    = azurerm_network_interface.PublicNIC[each.key].id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.publiclbpool.id
+  ip_configuration_name = each.value
+}
